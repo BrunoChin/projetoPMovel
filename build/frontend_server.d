5088e766d4a922e8a8a31bd597cc993e4e48a2cc
ShiroeChin <brunoholiveira996@gmail.com>
2019-08-21 17:25:42 -0300

    Finalização parcial do progeto

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 frontend_server.d: /home/aluno/develop/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
-=======
-frontend_server.d: C:\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
->>>>>>> de20dd7d798191f73ed592955025a9abfe5d6ef9
--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,15 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-String localZone;
 var resBody;
-var resBodyZones;
-List zonas;
 
-void main() => runApp(MaterialApp(
-  theme: new ThemeData(
-    brightness:Brightness.dark,
-  ),
-  home: Home(),
-));
+void main() =>
+    runApp(MaterialApp(
+      theme: new ThemeData(
+        brightness: Brightness.light,
+      ),
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -21,23 +19,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String localZone = "America/Maceio";
 
-  @override
-  void initState() {
-    super.initState();
-    localZone = "America/Maceio";
+  var resBodyZones;
+
+  TextEditingController _localControler = TextEditingController();
+
+  void _setZone(String text){
+    setState(() {
+      localZone = _localControler.text;
+    });
+    print(_localControler.toString());
   }
 
-  Future<Map> _getDataZoneTime() async{
-    http.Response resposta = await http.get("http://api.timezonedb.com/v2.1/get-time-zone?key=2OAR4HS329MU&format=json&by=zone&zone=$localZone");
+  Future<Map> _getDataZoneTime() async {
+    http.Response resposta = await http.get(
+        "http://api.timezonedb.com/v2.1/get-time-zone?key=2OAR4HS329MU&format=json&by=zone&zone=$localZone");
 
     resBody = json.decode(resposta.body);
 
     return resBody;
   }
 
-  Future<Map> _getDataZones() async{
-    http.Response resposta = await http.get("http://api.timezonedb.com/v2.1/list-time-zone?key=2OAR4HS329MU&format=json");
+  Future<Map> _getDataZones() async {
+    http.Response resposta = await http.get(
+        "http://api.timezonedb.com/v2.1/list-time-zone?key=2OAR4HS329MU&format=json");
 
     resBodyZones = json.decode(resposta.body);
 
@@ -50,18 +56,19 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("No seu Tempo"),
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text("No Seu Tempo"),
       ),
       body: FutureBuilder<Map>(
         future: _getDataZoneTime(),
-        builder: (context, snapshot){
+        builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
             case ConnectionState.none:
               return Center(
                 child: Text(
                   "Carragando os Dados....",
-                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
                 ),
               );
             default:
@@ -83,37 +90,37 @@ class _HomeState extends State<Home> {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
                         new Container(
-                          child:
-                          new Text(
-                            "$localZone",
-                            style: new TextStyle(fontSize:37.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w200,
-                                fontFamily: "Roboto"),
+                          child: Center(
+                            child: Text(
+                              "$localZone",
+                              style: new TextStyle(fontSize: 37.0,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w200,
+                                  fontFamily: "Roboto"),
+                            ),
                           ),
-
-                          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 1.0, 50.0),
+                          padding: const EdgeInsets.fromLTRB(
+                              0.0, 0.0, 1.0, 50.0),
                           alignment: Alignment.topLeft,
                         ),
 
                         new Container(
                           child:
-                          new Text(
-                            resBody["formatted"],
-                            style: new TextStyle(fontSize:35.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w200,
-                                fontFamily: "Roboto"),
-                          ),
-
+                          texto(),
                           padding: const EdgeInsets.all(0.0),
                           alignment: Alignment.bottomCenter,
                         ),
-
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+                          child: TextField(
+                            decoration: InputDecoration(labelText: "Buscar Local", border: OutlineInputBorder()),
+                            controller: _localControler,
+                            onSubmitted: _setZone,
+                          ),
+                          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                        ),
                       ]
-
                   ),
-
                 );
               }
           }
@@ -127,9 +134,9 @@ class _HomeState extends State<Home> {
           ),
           FutureBuilder<Map>(
             future: _getDataZones(),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-
-              if(snapshot.connectionState == ConnectionState.waiting || snapshot.connectionState == ConnectionState.none){
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting ||
+                  snapshot.connectionState == ConnectionState.none) {
                 return Container(
                     child: Center(
                       child: Text("Carredando..."),
@@ -139,18 +146,19 @@ class _HomeState extends State<Home> {
               return Expanded(
                 child: ListView.builder(
                     itemCount: 425,
-                    itemBuilder: (BuildContext context, int index){
-                      if(resBodyZones["zones"][index]["zoneName"] != null){
+                    itemBuilder: (BuildContext context, int index) {
+                      if (resBodyZones["zones"][index]["zoneName"] != null) {
                         return ListTile(
                           title: Text(resBodyZones["zones"][index]["zoneName"]),
-                          onLongPress: (){
+                          onLongPress: () {
                             setState(() {
-                              localZone = resBodyZones["zones"][index]["zoneName"];
+                              localZone =
+                              resBodyZones["zones"][index]["zoneName"];
                             });
                           },
                         );
                       }
-                      else{
+                      else {
                         return Text("Erro ao carrecar :(");
                       }
                     }),
@@ -159,6 +167,28 @@ class _HomeState extends State<Home> {
           ),
         ],),
       ),
+    );
+  }
+}
+
+Widget texto(){
+  if(resBody["formatted"].toString() == "" || resBody["formatted"] == null){
+    return Text(
+        "Local não encontrado",
+        style: new TextStyle(fontSize: 35.0,
+        color: Colors.black,
+        fontWeight: FontWeight.w200,
+        fontFamily: "Roboto"),
+    );
+  }
+  else{
+    return new Text(
+      resBody["formatted"],
+      style: new TextStyle(
+          fontSize: 35.0,
+          color: Colors.black,
+          fontWeight: FontWeight.w200,
+          fontFamily: "Roboto"),
     );
   }
 }
